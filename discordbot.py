0dee99ce8a570f1e873b555298b1cdd26a91d54d
kumicho <when0846cry@gmail.com>
2022-09-26 18:52:38 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -68,12 +68,12 @@ async def on_message(message):
            dic_file.close()
            await message.channel.send(message.content.replace(".del ","") + "を辞書から削除しました。")
            print("[  log  ]辞書削除 : " + message.content.replace(".del ",""))
-        input_Message = message.content
-        dic_file = open("dictionary.dic","r")
-        dic_file_read = dic_file.read()
-        dic_file_read = dic_file_read.split("[SPL]")
-        dic_num = len(dic_file_read)
-        for i in range(dic_num - 1):
+       input_Message = message.content
+       dic_file = open("dictionary.dic","r")
+       dic_file_read = dic_file.read()
+       dic_file_read = dic_file_read.split("[SPL]")
+       dic_num = len(dic_file_read)
+       for i in range(dic_num - 1):
             dic_file_read_Temp = dic_file_read[i].split(" = ")
             Text_A = dic_file_read_Temp[0]
             Text_B = dic_file_read_Temp[1]
