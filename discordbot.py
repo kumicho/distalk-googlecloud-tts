78b223f3efdee4fa55d848d6e461709f53dc3a66
kumicho <when0846cry@gmail.com>
2022-09-27 11:44:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -108,7 +108,7 @@ async def on_message(message):
            dic_file.close()
            await message.channel.send(message.content.replace(".del ","") + "を辞書から削除しました。")
            print("[  log  ]辞書削除 : " + message.content.replace(".del ",""))
-             await bot.process_commands(message)
+           await bot.process_commands(message)
        input_Message = message.content
        dic_file = open("dictionary.dic","r")
        dic_file_read = dic_file.read()
@@ -119,7 +119,7 @@ async def on_message(message):
             Text_A = dic_file_read_Temp[0]
             Text_B = dic_file_read_Temp[1]
             input_Message = input_Message.replace(Text_A,Text_B)
-             await bot.process_commands(message)
+            await bot.process_commands(message)
             
 @client.event
 async def on_message(message):
