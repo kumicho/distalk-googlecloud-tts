0b84e9e67a79dbe806fd368e3f111be2b6c174b5
kumicho <when0846cry@gmail.com>
2022-09-27 13:47:00 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -224,9 +224,6 @@ async def on_message(message):
            dic_file.close()
            await message.channel.send(input_Text + "を辞書に追加しました。")
            print("[  log  ]辞書追加 : " + input_Text)
-           await bot.process_commands(message)
-            
-
 
 
 client.run(token)
